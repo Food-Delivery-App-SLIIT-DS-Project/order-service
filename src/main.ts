2715d08761d287d03e80e21d75891459bf21d009
sajeebro <sajithmadusanka720@gmail.com>
2025-04-01 15:33:09 +0530

    initial

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        protoPath: join(__dirname, '../order.proto'),
+        package: 'order',
+        url: 'localhost:50056',
+      },
+    },
+  );
+  app.enableShutdownHooks();
+  await app.listen();
+  console.log('Order service is running on: http://localhost:50056');
+}
+void bootstrap();
