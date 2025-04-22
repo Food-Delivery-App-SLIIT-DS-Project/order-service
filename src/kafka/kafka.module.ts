1e628897539b7e8552d817a437314426dbb5e64d
sajeebro <sajithmadusanka720@gmail.com>
2025-04-22 05:33:32 +0530

    updated

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,9 +1,11 @@
 // src/kafka/kafka.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     ClientsModule.register([
       {
         name: 'KAFKA_SERVICE',
--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,27 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { ORDER_PACKAGE_NAME } from './types';
 
 async function bootstrap() {
+  // Load environment variables
+  void ConfigModule.forRoot({ isGlobal: true });
+  const host = process.env.GRPC_HOST || 'localhost';
+  const port = process.env.GRPC_PORT || '50055';
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.GRPC,
       options: {
         protoPath: join(__dirname, '../order.proto'),
-        package: 'order',
-        url: '0.0.0.0:50055',
+        package: ORDER_PACKAGE_NAME,
+        url: `${host}:${port}`,
       },
     },
   );
   app.enableShutdownHooks();
   await app.listen();
-  console.log('Order service is running on: http://localhost:50055');
+  console.log(`Order service is running on: grpc://${host}:${port}`);
 }
 void bootstrap();
