5210ded07098cbbe0414a7b72d4ec41a754b6f7c
sajeebro <sajithmadusanka720@gmail.com>
2025-04-23 11:50:17 +0530

    docker running v1

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,7 @@ import { ORDER_PACKAGE_NAME } from './types';
 async function bootstrap() {
   // Load environment variables
   void ConfigModule.forRoot({ isGlobal: true });
-  const host = process.env.GRPC_HOST || 'localhost';
-  const port = process.env.GRPC_PORT || '50055';
+  const url = process.env.ORDER_SERVICE_URL || 'localhost:50054';
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
@@ -17,12 +16,12 @@ async function bootstrap() {
       options: {
         protoPath: join(__dirname, '../order.proto'),
         package: ORDER_PACKAGE_NAME,
-        url: `${host}:${port}`,
+        url: url, // order-service
       },
     },
   );
   app.enableShutdownHooks();
   await app.listen();
-  console.log(`Order service is running on: grpc://${host}:${port}`);
+  console.log(`Order service is running on ${url}`);
 }
 void bootstrap();
